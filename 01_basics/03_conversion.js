eb5b1e26df5c44d6106d0f1436130a6e437e9112
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-06 11:10:20 +0000

    everything about type conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,11 @@
+/*
+here this can be used to convert any type of function into any like
+if my value is in string but i wqant to get into integer can do easily
+*/
+let myName = "Divyang"
+let number = 34
+let myname = Boolean (myName)
+let originalnum = String(number)
+console.log(typeof (originalnum));
+console.log(myname)
+console.log(typeof (myname))
