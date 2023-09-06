a467cd4e576d5e06508bd5436f0bff565d0734cf
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-06 10:34:43 +0000

    01_variable.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "divakki07@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gujarat"
+/*const means that is un-modified !
+for declaring variable let is used more than var
+var is not used due to the issue in block scoping and functional scoping .
+*/
+ accountEmail = "divyangchhantbar@gmail.com"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
