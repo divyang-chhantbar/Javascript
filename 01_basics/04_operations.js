77441db9ce075fe0f61a04930641a09dbf5bafe1
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-07 08:12:14 +0000

    operations and comparisions

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,20 @@
+/* yaha par operations , operator and baatein hein comparisions ki 
+halaki kuch operations zara sa bhi use nai krte jese null and undefined ki comparisions */
+str1 = "Hello"
+str2 = " Divyang"
+str3 = str1 + str2
+console.log(2+2);
+console.log(10%2);
+console.log("2" + 2);
+console.log(str3);
+console.log(2>1);
+console.log("-11"< 0);
+
+/* some miscellaneous comparisions */
+console.log(null<0);
+console.log(null>=0);
+console.log(null==0);
+
+console.log(undefined==0);
+console.log(undefined<=0);
+console.log(undefined>=0);
