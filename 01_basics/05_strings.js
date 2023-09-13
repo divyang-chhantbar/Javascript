22aae4e07286497933ef311fb440c796882d30e7
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-13 03:58:12 +0000

    ABC's of string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+// let's study about the string
+const myName = "Divyang"
+const mySurname = "Chhantbar"
+const myFullname = myName + mySurname
+//traditional way
+console.log(myFullname + "I live in Morbi" + "Pursuing computer science");
+//modern way
+console.log(`Hello my name is ${myFullname} and I live in Morbi right now pursuing computer science`);
+console.log(myFullname.length);
+console.log(myFullname.charAt(5));
+console.log(myFullname.toUpperCase);
+console.log(myFullname.indexOf('C'));
