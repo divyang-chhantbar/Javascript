8075870d2e127b4960c6061f16d633c988e4d001
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-13 12:28:32 +0000

    ABC's of Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,10 +3,13 @@ const myName = "Divyang"
 const mySurname = "Chhantbar"
 const myFullname = myName + mySurname
 //traditional way
-console.log(myFullname + "I live in Morbi" + "Pursuing computer science");
+console.log(myFullname + " I live in Morbi" + " Pursuing computer science");
 //modern way
 console.log(`Hello my name is ${myFullname} and I live in Morbi right now pursuing computer science`);
 console.log(myFullname.length);
 console.log(myFullname.charAt(5));
 console.log(myFullname.toUpperCase);
 console.log(myFullname.indexOf('C'));
+console.log(myFullname.substring(0,4));
+console.log(myFullname.slice(-10,7));
+console.log(myFullname.replace('Divyang' , 'Div')); 
