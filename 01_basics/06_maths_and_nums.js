01b9079efa1acb971c80f867cde333e0daac52ab
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-13 13:18:35 +0000

    some basicsof maths functions

---
+++ 01_basics/06_maths_and_nums.js
@@ -0,0 +1,19 @@
+const myNum = 1000000
+const anotherNum = 123.874
+const anum = new Number (500)
+console.log(myNum);
+console.log(num);
+console.log(myNum.toLocaleString('en-IN'));
+console.log(anotherNum.toFixed(2));
+console.log(anotherNum.toPrecision(3));
+
+// ++++++++++++++++++++++++++ MATHS +++++++++++++++++++++
+
+const Number = 153.56
+
+// console.log(Math);
+// console.log(Math.round(Number));
+// console.log(Math.abs(-100));
+// console.log(Math.min(4,3,2,1));
+// console.log(Math.max(4,3,2,1));
+console.log(Math.random());
