24842fbe6a75f572b6921d40640c101db4acbb2f
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-24 10:44:57 +0000

    switch case

---
+++ 04_controlflow/03_switch.js
@@ -0,0 +1,30 @@
+// Here the usage of switches and implementation are done and below is the syntax . .  
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "january":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
