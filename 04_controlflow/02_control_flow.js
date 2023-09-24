4b2bdd2993677a7e54f9eb06d8d51dca6b960d3b
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-24 09:48:02 +0000

    Conditions and Conditional operator .

--- 04_controlflow/02_control_flow.js
+++ 04_controlflow/02_control_flow.js
@@ -43,8 +43,9 @@ const debitCard = true
 const loggedInFromGoogle = false
 const loggedInFromEmail = true
 
-// 
-
+// if (userLoggedIn && loggedInFromGoogle && 3==3) {
+//     console.log("Allow to buy course");
+// }
 
 if (loggedInFromGoogle || loggedInFromEmail) {
     console.log("User logged in");
