058aa24d0ba9d5c983294688cbe60e01b784f9e5
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-09-25 16:12:31 +0000

    loop discussion

---
+++ 05_iterations/02_do_while.js
@@ -0,0 +1,22 @@
+ let index = 0
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     // index = index + 2
+//     index+=2
+// }
+
+let myArray = ["Divyang","Heet","Meet"]
+
+let arr = 0 // Initialization
+while (arr< myArray.length ){ // Condition
+    console.log(`Value of my array is ${myArray[arr]}`);
+    arr = arr+1 // Updation
+}
+
+let score = 11 // initialization
+
+do{
+    console.log(`score is ${score}`);
+    score++ // updation
+}
+while(score <= 10 );
