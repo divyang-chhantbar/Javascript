3d78315583a60af172e349a302a25261a66a3909
Divyang Chhantbar <divyangchhantbar@gmail.com>
2023-11-03 10:23:13 +0000

    basic of events

--- 08_events/01_eventbasic.js
+++ 08_events/01_eventbasic.js
@@ -1,1 +1,7 @@
 console.log("Getting Started with EVENTS");
+/* Events are the motion or the moments happening over the webpages whether its the cursor or you make the changes or scrolling or the clicks etc
+for that we mostly play with the event listeners and give the function to them to do particular task 
+there's one concept of EVENT PROPOGATION where we can know how our code of events getting executed , two types of propogation
+1) Bubbling : child node to parent node 
+2) Capturing : opposite of the above 
+*/
